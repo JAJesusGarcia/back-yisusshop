aee59f3850626a675a9c7e662b5e378a72617940
JAJesusGarcia <jesusjagarcia98@gmail.com>
2024-10-10 17:06:26 -0300

    dataBase configg

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -5,24 +5,24 @@ import { Order } from '../entities/Order';
 import { Category } from '../entities/Category';
 import { Product } from '../entities/Product';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  url: process.env.DATABASE_URL, // Usar DATABASE_URL aquí
-  synchronize: true,
-  logging: false,
-  entities: [User, Credential, Order, Product, Category],
-  subscribers: [],
-  migrations: [],
-});
-
 // export const AppDataSource = new DataSource({
 //   type: 'postgres',
-//   url: process.env.DATABASE_URL, // Usa la URL completa
+//   url: process.env.DATABASE_URL, // Usar DATABASE_URL aquí
 //   synchronize: true,
 //   logging: false,
-//   ssl: true,
 //   entities: [User, Credential, Order, Product, Category],
+//   subscribers: [],
+//   migrations: [],
 // });
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL, // Usa la URL completa
+  synchronize: true,
+  logging: false,
+  ssl: true,
+  entities: [User, Credential, Order, Product, Category],
+});
 
 // export const AppDataSource = new DataSource({
 //   type: 'postgres',
